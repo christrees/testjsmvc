4a6cae9bb9c3c37dcf0783f2c660c9744ebae386
Chris <christrees@yahoo.com>
2011-07-26 09:54:51 -0500

    failing funcunit on cookbook

--- cookbook/test/funcunit/funcunit.js
+++ cookbook/test/funcunit/funcunit.js
@@ -1,2 +1,3 @@
-steal("funcunit")
- .then("./cookbook_test.js");
+steal("cookbook/test/funcunit/funcunit.js")
+ .then("cookbook/test/funcunit/cookbook_test.js")
+ .then("cookbook/test/funcunit/recipe_controller_test.js");
